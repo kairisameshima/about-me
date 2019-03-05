777871bdb0258fc19b6d46bed5dc29ffe90d7875
Kairi Sameshima <kairi@chicoryapp.com>
2019-03-05 14:12:47 -0500

    Updates

---
+++ precache-manifest.d1eb564758b1c4ff20233e02f860d1b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57e7325969342a5fad78",
+    "url": "/about-me/static/css/main.8723dafa.chunk.css"
+  },
+  {
+    "revision": "57e7325969342a5fad78",
+    "url": "/about-me/static/js/main.57e73259.chunk.js"
+  },
+  {
+    "revision": "75c397aa21cc7d5ba922",
+    "url": "/about-me/static/js/runtime~main.75c397aa.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/about-me/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about-me/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2d30977238ca6fc9662f8a1051e29f7c",
+    "url": "/about-me/index.html"
+  }
+];
